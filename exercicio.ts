d7b32e9590877820433dd615c632478ede3bcb96
Lucas-Marcelo <sonsofmaria@outlook.com>
2019-08-07 21:59:19 -0300

    inclusão do if no arquivo .ts

---
+++ exercicio.ts
@@ -0,0 +1,16 @@
+let nota1: number = 10
+let nota2: number = 9
+let nota3: number = 5
+
+let media: number = (nota1 + nota2 + nota3) / 3
+
+console.log("A média é: " + media)
+
+if(media >= 7)
+{
+    console.log("APROVADO MANN ")
+}
+else
+{
+    console.log("REPROVADO BROO ")
+}
